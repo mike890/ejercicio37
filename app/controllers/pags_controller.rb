54bbcd6338d0affbfb653e2e94497bab743593ab
Mike <mapv890@gmail.com>
2017-03-21 22:33:17 -0600

    un commit mas del taller sin hacer el punto 34

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,16 @@
 class PagsController < ApplicationController
+  before_action :get_data
+
   def pag1
+    
   end
 
   def pag2
+    render :layout => 'landing'
+  end
+  def pag3
+  end
+  def get_data
+    @users= ['alumno1', 'alumno2', 'alumno3']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'pags/pag2'
 
+  get 'pags/pag3'
+
   root 'pags#pag1'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
